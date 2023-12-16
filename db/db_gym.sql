0856caa7ecc61ea21cc4d7fa2637fe82b27edb97
RaixsG <jhardelloficial@gmail.com>
2023-12-16 05:17:01 -0500

    Update user authentication logic

--- db/db_gym.sql
+++ db/db_gym.sql
@@ -2,9 +2,9 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 04-12-2023 a las 17:06:08
--- Versión del servidor: 8.0.34
+-- Servidor: localhost:3306
+-- Tiempo de generación: 16-12-2023 a las 10:16:38
+-- Versión del servidor: 8.0.35
 -- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clientes` (
   `ID_cliente` int NOT NULL,
-  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `apellido` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `direccion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telefono` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `fecha_nacimiento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -48,7 +48,7 @@ INSERT INTO `clientes` (`ID_cliente`, `nombre`, `apellido`, `direccion`, `telefo
 (4, 'Pancho', 'Apuela', 'Jr. Apuela', '123456789', 'apuela@apuela.com', '2003-06-17'),
 (5, 'Prueba 1', 'Prueba 1', 'Prueba 1', 'Prueba 32', 'Prueba 4', '2002-01-01'),
 (6, 'Su', 'Apuela', 'Jr. Apuela', '741258963', '2022200042@ucss.pe', '2005-01-11'),
-(7, 'General01', 'General01', 'General01', 'General01', 'General01@General01.com', '2000-01-01');
+(7, 'General', 'General', 'General', 'General', 'General01@General01.com', '2000-01-01');
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,34 @@ CREATE TABLE `detalles_venta` (
 INSERT INTO `detalles_venta` (`ID_detalle_venta`, `cantidad`, `ID_venta`, `ID_producto`) VALUES
 (1, 5, 1, 1),
 (2, 3, 2, 1),
-(3, 5, 3, 2);
+(3, 5, 3, 2),
+(4, 2, 4, 1),
+(5, 4, 4, 2),
+(6, 5, 5, 1),
+(7, 24, 5, 3),
+(8, 23, 6, 3),
+(9, 22, 7, 3),
+(10, 22, 7, 3),
+(11, 21, 8, 3),
+(12, 4, 9, 1),
+(13, 20, 10, 3),
+(14, 19, 11, 3),
+(15, 18, 12, 3),
+(16, 16, 13, 3),
+(17, 14, 14, 3),
+(18, 2, 16, 3),
+(19, 2, 17, 3),
+(20, 1, 18, 3),
+(21, 1, 19, 3),
+(22, 1, 20, 3),
+(23, 1, 21, 3),
+(24, 1, 22, 3),
+(25, 1, 23, 3),
+(26, 1, 24, 3),
+(27, 2, 25, 3),
+(28, 2, 26, 3),
+(29, 1, 27, 3),
+(30, 1, 28, 2);
 
 -- --------------------------------------------------------
 
@@ -90,9 +117,11 @@ CREATE TABLE `enseña_en` (
 
 INSERT INTO `enseña_en` (`ID_enseña_en`, `ID_horario`, `ID_instructor`) VALUES
 (1, 1, 1),
-(2, 1, 1),
-(3, 2, 2),
-(4, 3, 2);
+(2, 2, 1),
+(3, 4, 2),
+(4, 3, 2),
+(5, 1, 3),
+(6, 3, 5);
 
 -- --------------------------------------------------------
 
@@ -104,7 +133,7 @@ CREATE TABLE `horarios` (
   `ID_horario` int NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_finalizacion` time NOT NULL,
-  `turno` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `turno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -112,9 +141,11 @@ CREATE TABLE `horarios` (
 --
 
 INSERT INTO `horarios` (`ID_horario`, `hora_inicio`, `hora_finalizacion`, `turno`) VALUES
-(1, '05:30:00', '11:59:00', 'Mañana'),
-(2, '12:00:00', '16:59:00', 'Tarde'),
-(3, '17:00:00', '21:00:00', 'Noche');
+(1, '05:30:00', '12:59:00', 'Mañana'),
+(2, '13:00:00', '16:59:00', 'Media Tarde'),
+(3, '17:00:00', '21:00:00', 'Noche'),
+(4, '05:30:00', '09:00:00', 'Media mañana'),
+(6, '21:00:00', '23:59:00', 'Media Noche');
 
 -- --------------------------------------------------------
 
@@ -134,10 +165,12 @@ CREATE TABLE `inscripciones` (
 --
 
 INSERT INTO `inscripciones` (`ID_inscripcion`, `fecha_inscripcion`, `ID_cliente`, `ID_enseña_en`) VALUES
-(1, '2023-12-02', 6, 3),
 (2, '2023-12-02', 5, 3),
 (3, '2023-12-01', 2, 3),
-(4, '2023-12-01', 3, 2);
+(4, '2023-12-01', 3, 2),
+(5, '2023-12-06', 1, 1),
+(6, '2023-12-06', 4, 2),
+(7, '2023-03-01', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -147,13 +180,13 @@ INSERT INTO `inscripciones` (`ID_inscripcion`, `fecha_inscripcion`, `ID_cliente`
 
 CREATE TABLE `instructores` (
   `ID_instructor` int NOT NULL,
-  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `apellido` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `direccion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telefono` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `fecha_nacimiento` date NOT NULL,
-  `especializacion` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `especializacion` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `foto_instructor` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -163,7 +196,9 @@ CREATE TABLE `instructores` (
 
 INSERT INTO `instructores` (`ID_instructor`, `nombre`, `apellido`, `direccion`, `telefono`, `email`, `fecha_nacimiento`, `especializacion`, `foto_instructor`) VALUES
 (1, 'Instructor 1', 'Instructor 1', 'Instructor 1', '975315984', 'jhardelloficial@gmail.com', '1985-06-12', 'Cardio', NULL),
-(2, 'Instructor 2', 'Instructor 2', 'Instructor 2', '923145766', 'Instructor2@Instructor2.com', '1991-11-12', 'Peso', NULL);
+(2, 'Instructor 2', 'Instructor 2', 'Instructor 2', '923145766', 'Instructor2@Instructor2.com', '1991-12-11', 'Full Peso', NULL),
+(3, 'Instructor 3', 'Instructor 3', 'Instructor 3', '862497135', 'instructor3@Instructor3.com', '1983-06-15', 'Danza', NULL),
+(5, 'Jose Fernando', 'Rodrigues Rodrigo', 'Jr. Serafin Filomeno', '33333333', 'elmasfit@gmail.com', '2003-06-18', 'Aerobicos ', NULL);
 
 -- --------------------------------------------------------
 
@@ -173,9 +208,9 @@ INSERT INTO `instructores` (`ID_instructor`, `nombre`, `apellido`, `direccion`, 
 
 CREATE TABLE `membresias` (
   `ID_membresia` int NOT NULL,
-  `nombre_membresia` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre_membresia` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `beneficios` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+  `beneficios` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -184,7 +219,8 @@ CREATE TABLE `membresias` (
 
 INSERT INTO `membresias` (`ID_membresia`, `nombre_membresia`, `precio`, `beneficios`) VALUES
 (1, 'Normal', 100.00, 'GYM + BAILE'),
-(2, 'VIP', 110.00, 'GYM + BAILE + AGUA');
+(2, 'VIP', 110.00, 'GYM + BAILE + AGUA'),
+(3, 'Ahorrador', 80.00, 'GYM POR LA MAÑANA');
 
 -- --------------------------------------------------------
 
@@ -205,9 +241,12 @@ CREATE TABLE `miembros` (
 --
 
 INSERT INTO `miembros` (`ID_miembro`, `estado_inscripcion`, `ID_membresia`, `ID_cliente`, `fecha_vencimiento`) VALUES
-(1, 1, 1, 5, '2023-12-02'),
+(1, 1, 1, 5, '2024-03-02'),
 (2, 1, 1, 2, '2023-12-02'),
-(3, 1, 1, 3, '2023-12-02');
+(3, 1, 1, 3, '2024-02-02'),
+(4, 1, 1, 1, '2023-12-06'),
+(5, 1, 2, 4, '2024-01-06'),
+(6, 1, 2, 6, '2024-01-16');
 
 -- --------------------------------------------------------
 
@@ -219,9 +258,19 @@ CREATE TABLE `pago_membresias` (
   `ID_pago_mem` int NOT NULL,
   `fecha_pago` date NOT NULL,
   `cantidad` int NOT NULL,
-  `metodo_pago` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metodo_pago` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ID_miembro` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `pago_membresias`
+--
+
+INSERT INTO `pago_membresias` (`ID_pago_mem`, `fecha_pago`, `cantidad`, `metodo_pago`, `ID_miembro`) VALUES
+(1, '2023-12-09', 3, 'efectivo', 1),
+(2, '2023-12-10', 2, 'efectivo', 3),
+(3, '2023-12-16', 1, 'efectivo', 6),
+(4, '2023-12-16', 1, 'tarjeta', 5);
 
 -- --------------------------------------------------------
 
@@ -231,8 +280,8 @@ CREATE TABLE `pago_membresias` (
 
 CREATE TABLE `productos` (
   `ID_producto` int NOT NULL,
-  `nombre_producto` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descripcion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre_producto` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `cantidad_stock` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -242,8 +291,10 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`ID_producto`, `nombre_producto`, `descripcion`, `precio`, `cantidad_stock`) VALUES
-(1, 'Proteina', 'Proteina de calidad', 150.00, 2),
-(2, 'Trembolona', 'Pa ser más fuertes', 500.00, 4);
+(1, 'Proteina', 'Proteina de calidad', 150.00, 10),
+(2, 'Trembolona', 'Pa ser más fuertes', 500.00, 2),
+(3, 'Agua San Luis', 'Agua San Luis Mineral', 2.50, 7),
+(5, 'Suplemento', 'Suplmento CR3', 52.50, 100);
 
 -- --------------------------------------------------------
 
@@ -253,8 +304,8 @@ INSERT INTO `productos` (`ID_producto`, `nombre_producto`, `descripcion`, `preci
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `username` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -273,7 +324,7 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 CREATE TABLE `venta` (
   `ID_venta` int NOT NULL,
   `fecha_pago` date NOT NULL,
-  `metodo_pago` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metodo_pago` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ID_cliente` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -284,7 +335,32 @@ CREATE TABLE `venta` (
 INSERT INTO `venta` (`ID_venta`, `fecha_pago`, `metodo_pago`, `ID_cliente`) VALUES
 (1, '2023-12-02', 'efectivo', 1),
 (2, '2023-12-02', 'efectivo', 7),
-(3, '2023-12-02', 'tarjeta', 7);
+(3, '2023-12-02', 'tarjeta', 7),
+(4, '2023-12-07', 'efectivo', 6),
+(5, '2023-12-10', 'efectivo', 7),
+(6, '2023-12-10', 'efectivo', 7),
+(7, '2023-12-10', 'efectivo', 7),
+(8, '2023-12-10', 'tarjeta', 7),
+(9, '2023-12-10', 'tarjeta', 7),
+(10, '2023-12-10', 'efectivo', 7),
+(11, '2023-12-10', 'efectivo', 7),
+(12, '2023-12-10', 'efectivo', 1),
+(13, '2023-12-10', 'efectivo', 1),
+(14, '2023-12-10', 'efectivo', 1),
+(15, '2023-12-10', 'efectivo', 1),
+(16, '2023-12-10', 'efectivo', 1),
+(17, '2023-12-10', 'efectivo', 1),
+(18, '2023-12-10', 'efectivo', 7),
+(19, '2023-12-10', 'efectivo', 7),
+(20, '2023-12-10', 'tarjeta', 7),
+(21, '2023-12-10', 'efectivo', 7),
+(22, '2023-12-10', 'efectivo', 7),
+(23, '2023-12-10', 'efectivo', 7),
+(24, '2023-12-10', 'tarjeta', 7),
+(25, '2023-12-10', 'efectivo', 1),
+(26, '2023-12-10', 'efectivo', 1),
+(27, '2023-12-16', 'efectivo', 7),
+(28, '2023-12-16', 'efectivo', 7);
 
 -- --------------------------------------------------------
 
@@ -293,11 +369,11 @@ INSERT INTO `venta` (`ID_venta`, `fecha_pago`, `metodo_pago`, `ID_cliente`) VALU
 --
 
 CREATE TABLE `_prisma_migrations` (
-  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `checksum` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `checksum` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `finished_at` datetime(3) DEFAULT NULL,
-  `migration_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `logs` text COLLATE utf8mb4_unicode_ci,
+  `migration_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logs` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `rolled_back_at` datetime(3) DEFAULT NULL,
   `started_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
   `applied_steps_count` int UNSIGNED NOT NULL DEFAULT '0'
@@ -428,61 +504,61 @@ ALTER TABLE `_prisma_migrations`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `ID_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `detalles_venta`
 --
 ALTER TABLE `detalles_venta`
-  MODIFY `ID_detalle_venta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_detalle_venta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `enseña_en`
 --
 ALTER TABLE `enseña_en`
-  MODIFY `ID_enseña_en` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_enseña_en` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `ID_horario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_horario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `inscripciones`
 --
 ALTER TABLE `inscripciones`
-  MODIFY `ID_inscripcion` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_inscripcion` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `instructores`
 --
 ALTER TABLE `instructores`
-  MODIFY `ID_instructor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_instructor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `membresias`
 --
 ALTER TABLE `membresias`
-  MODIFY `ID_membresia` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_membresia` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `miembros`
 --
 ALTER TABLE `miembros`
-  MODIFY `ID_miembro` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_miembro` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `pago_membresias`
 --
 ALTER TABLE `pago_membresias`
-  MODIFY `ID_pago_mem` int NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_pago_mem` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `ID_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -494,7 +570,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `ID_venta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_venta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Restricciones para tablas volcadas
